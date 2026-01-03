ea505058a3f5d106618d0be0ddd087d8e917af3c
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2026-01-03 01:28:04 -0500

    Update set_locale.php
    Signed-off-by: ArchBlood <35392110+ArchBlood@users.noreply.github.com>

--- example/set_locale.php
+++ example/set_locale.php
@@ -1,5 +1,32 @@
 <?php
 
+/*
+ * ===============================================================
+ *      ____        _       _      _      _  _____ 
+ *     / __ \      (_)     (_)    (_)    | |/ ____|
+ *    | |  | |_ __  _  __ _ _ _ __ _     | | (___  
+ *    | |  | | '_ \| |/ _` | | '__| |_   | |\___ \ 
+ *    | |__| | | | | | (_| | | |  | | |__| |____) |
+ *     \____/|_| |_|_|\__, |_|_|  |_|\____/|_____/ 
+ *                     __/ |                       
+ *                    |___/                        
+ * ===============================================================
+ *
+ *   Lightweight, deliciously simple, modular JavaScript framework for building reactive HumHub modules with enterprise-grade security
+ *
+ *   Website:   https://onigirijs.greenmeteor.net/
+ *   License:   BSD-3-Clause
+ *
+ *   Copyright (c) 2025 Green Meteor
+ *
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the
+ *   conditions of the BSD 3-Clause License are met.
+ *
+ *   SPDX-License-Identifier: BSD-3-Clause
+ * ===============================================================
+ */
+
 session_start();
 
 header('Content-Type: application/json');
